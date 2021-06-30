b3beb9e6408d21cb6230a535317f434b9a1dc288
LunaMellow <nore0404@gmail.com>
2021-06-30 03:43:58 +0200

    Logo path added

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createWindow () {
   win = new BrowserWindow({
     width: 600, 
     height: 670,
-    icon: `file://${__dirname}dist\redux\Redux Logo.png`
+    icon: `file://${__dirname}src/assets/logo.png`
   })
 
   win.loadURL(`file://${__dirname}/dist/index.html`)
